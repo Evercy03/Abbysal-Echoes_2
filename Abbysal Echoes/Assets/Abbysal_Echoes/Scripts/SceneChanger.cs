33e305e0f32309dda3de24ef9a97e41d458abf1c
Eletronsky <elenaencinassanchez1@gmail.com>
2025-05-22 13:16:22 +0200

    Script de SceneChanger

--- Abbysal Echoes/Assets/Abbysal_Echoes/Scripts/SceneChanger.cs
+++ Abbysal Echoes/Assets/Abbysal_Echoes/Scripts/SceneChanger.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool gameEnded = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!gameEnded)
+        {
+            CheckBossDeath();
+        }
+    }
+
+    // Este método debe ser llamado desde el script que controle la muerte del jugador
+    public void OnPlayerDeath()
+    {
+        if (!gameEnded)
+        {
+            gameEnded = true;
+            LoadScene("You Loose");
+        }
+    }
+
+    void CheckBossDeath()
+    {
+        GameObject boss = GameObject.FindGameObjectWithTag("Boss");
+
+        if (boss == null)
+        {
+            gameEnded = true;
+            LoadScene("You Win");
+        }
+    }
+
+    void LoadScene(string sceneName)
+    {
+        Debug.Log("Cambiando a escena: " + sceneName);
+        SceneManager.LoadScene(sceneName);
     }
 }
