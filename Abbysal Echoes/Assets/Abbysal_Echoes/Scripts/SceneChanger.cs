4c7b411df05b76672e384bce1ddc2d39c9a2bb58
Eva Barrera Salcedo <evuski003@gmail.com>
2025-05-25 00:58:20 +0200

    SceneChanger Fixed

--- Abbysal Echoes/Assets/Abbysal_Echoes/Scripts/SceneChanger.cs
+++ Abbysal Echoes/Assets/Abbysal_Echoes/Scripts/SceneChanger.cs
@@ -15,21 +15,70 @@ public class SceneChanger : MonoBehaviour
     public string playerTag = "Player"; // Tag del jugador
 
     private bool bossDefeated = false;
+    [Header("Nombre de la escena del juego")]
+    public string gameSceneName = "NombreDeTuPrimeraEscena";
+
+    [Header("Nombre de la escena de créditos (opcional)")]
+    public string creditsSceneName = "CreditsScene";
+
+    private GameObject boss;
+    private bool bossWasFound = false;
+    
+
+    public void PlayGame()
+    {
+        Debug.Log("Cargando juego...");
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;  // Esto es para salir del modo juego en el editor
+#endif
+    }
+
+    public void LoadCredits()
+    {
+        Debug.Log("Cargando créditos...");
+        SceneManager.LoadScene(creditsSceneName);
+    }
 
     void Update()
     {
         // Verifica constantemente si el boss ha sido destruido
-        if (!bossDefeated && GameObject.FindGameObjectWithTag(bossTag) == null)
+        if (SceneManager.GetActiveScene().name == "MainMenuScene")
+            return;
+
+        // Buscar al boss solo una vez
+        if (!bossWasFound)
+        {
+            boss = GameObject.FindGameObjectWithTag(bossTag);
+            if (boss != null)
+            {
+                bossWasFound = true;
+            }
+        }
+
+        // Si ya fue encontrado y ahora está destruido
+        if (bossWasFound && !bossDefeated && boss == null)
         {
             bossDefeated = true;
             Debug.Log("Boss derrotado, cambiando a escena: " + sceneOnBossDeath);
             SceneManager.LoadScene(sceneOnBossDeath);
         }
+
+        // Comprobar muerte del jugador
         if (playerHealth != null && playerHealth.playerHealth <= 0)
         {
             SceneManager.LoadScene("LoseScene");
         }
     }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
