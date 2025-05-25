db1525963a21c9d0f4041ea1e6f7b3c0a262ace2
Eletronsky <elenaencinassanchez1@gmail.com>
2025-05-25 13:04:27 +0200

    Boss_2.0

--- Abbysal Echoes/Assets/Abbysal_Echoes/Scripts/Examples/Boss.cs
+++ Abbysal Echoes/Assets/Abbysal_Echoes/Scripts/Examples/Boss.cs
@@ -238,7 +238,7 @@ public class Boss : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(3);
+            SceneManager.LoadScene(4);
         }
     }
 }
